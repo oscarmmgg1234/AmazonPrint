c7e0041688bf24065d57a0c13361a47342393fc2
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2025-11-23 10:13:49 -0800

    Change database host in dbManger.js
    Updated database host for connection.

--- SRC/db/dbManger.js
+++ SRC/db/dbManger.js
@@ -2,7 +2,7 @@
 exports.query_manager = require("knex")({
   client: "mysql2",
   connection: {
-    host: "76.50.237.33",
+    host: "192.168.1.218",
     user: "oscar",
     port: 3306,
     password: "Omariscool1234!",
